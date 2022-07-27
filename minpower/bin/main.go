70bc5b1300362087c7e8d2e200c087e7b905289f
Jim Pick <jim@jimpick.com>
2022-07-27 07:51:32 -0700

    Lookup power via Lotus API

--- minpower/bin/main.go
+++ minpower/bin/main.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/filecoin-project/go-address"
 	jsonrpc "github.com/filecoin-project/go-jsonrpc"
 	lotusapi "github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func main() {
 	headers := http.Header{}
-	addr := "api.chain.love"
+	api_addr := "api.chain.love"
 
 	var api lotusapi.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(context.Background(),
-		"wss://"+addr+"/rpc/v0", "Filecoin",
+		"wss://"+api_addr+"/rpc/v0", "Filecoin",
 		[]interface{}{&api.Internal, &api.CommonStruct.Internal}, headers)
 	if err != nil {
 		log.Fatalf("connecting with lotus failed: %s", err)
 	}
 	defer closer()
 
-	// Now you can call any API you're interested in.
-	tipset, err := api.ChainHead(context.Background())
+	addr, err := address.NewFromString("f02620")
 	if err != nil {
-		log.Fatalf("calling chain head: %s", err)
+		log.Fatalf("address error: %s", err)
 	}
-	fmt.Printf("Current chain head is: %s", tipset.String())
+
+	// Now you can call any API you're interested in.
+	power, err := api.StateMinerPower(context.Background(), addr, types.EmptyTSK)
+	if err != nil {
+		log.Fatalf("calling state miner power: %s", err)
+	}
+	log.Println(power)
 }
