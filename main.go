7daa5420e4a462b15cb77450f28241cd14af7749
Jim Pick <jim@jimpick.com>
2022-08-02 08:34:11 -0700

    Exec go test for each miner

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
 type Miner struct {
@@ -19,13 +21,38 @@ func main() {
 	if err != nil {
 		log.Fatalln("Could not load responses from", responsesFile)
 	}
-	var responses []map[string]interface{}
+	// var responses []map[string]interface{}
+	var responses []map[string]string
 	json.Unmarshal(responsesJsonBytes, &responses)
 
 	for _, response := range responses {
 		for key, value := range response {
-			fmt.Println(key, value.(string))
+			fmt.Println(key, value)
+		}
+		i := 1
+		for {
+			minerID, ok := response[fmt.Sprintf("%d_minerid", i)]
+			if !ok {
+				break
+			}
+			city := response[fmt.Sprintf("%d_city", i)]
+			countrycode := response[fmt.Sprintf("%d_country_code", i)]
+			fmt.Println(i, minerID, city, countrycode)
+			miner := Miner{minerID, city, countrycode}
+			result, err := test_miner(context.Background(), miner)
+			fmt.Printf("Result %v %v\n", result, err)
+			i++
 		}
 		fmt.Println()
 	}
+}
+
+func test_miner(ctx context.Context, miner Miner) (bool, error) {
+	cmd := exec.CommandContext(ctx, "go", "test", "./minpower", "-json")
+	out, err := cmd.Output()
+	if err != nil {
+		return false, err
+	}
+	fmt.Println(string(out))
+	return true, nil
 }
